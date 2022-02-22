fa764c8135e09746cff96f04d59bd06351cdb63f
hajimalung <hajimalungbaba.786@gmail.com>
2022-02-22 19:47:44 +0530

    events binded btw cockpit and app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,20 @@ export class AppComponent {
   serverElements = [
     { type: "server", name: "test server", content: "this is test server!" },
   ];
+
+  onServerAdded(serverData: { serverName: string; serverContent: string }) {
+    this.serverElements.push({
+      type: "server",
+      name: serverData.serverName,
+      content: serverData.serverContent,
+    });
+  }
+
+  onBlueprintAdded(serverData) {
+    this.serverElements.push({
+      type: "blueprint",
+      name: serverData.serverName,
+      content: serverData.serverContent,
+    });
+  }
 }
